c745c550d2a64837cd8e80347297d4dddc86f27f
Emir <77904942+ReFo0@users.noreply.github.com>
2023-04-20 12:03:18 +0300

    Update tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -1,13 +1,9 @@
 from tiktokapi import TikTokApi
 
-# TikTok API'ya erişmek için kullanılacak kimlik bilgileri
 api = TikTokApi.get_instance(use_selenium=True)
 
-# Videonun URL'si
 video_url = "video link"
 
-# Videoyu almak için TikTok API'sini kullanın
 video_id = api.get_video_by_url(video_url)
 
-# Videoyu paylaşmak için TikTok API'sini kullanın
 api.upload_video("Yeniden paylaşılan video açıklaması", video_id)
